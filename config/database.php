7200d1a0ce307c550c0858b23d45537b723f0b16
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-09-19 16:59:53 +0600

    Added back jenssegers/laravel-mongodb

--- config/database.php
+++ config/database.php
@@ -91,6 +91,12 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_DSN'),
+            'database' => env('DB_DATABASE', 'homestead'),
+        ],
+
     ],
 
     /*
