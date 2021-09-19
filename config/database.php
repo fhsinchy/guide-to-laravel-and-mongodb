aa16488cdb328513027d98432282242a9f46c606
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-09-19 16:55:30 +0600

    Revert "Added jenssegers/laravel-mongodb, Laravel Breeze"
    This reverts commit 30e918f3832e524675ddbe5e5c16c931889bc982.

--- config/database.php
+++ config/database.php
@@ -91,14 +91,6 @@ return [
             'prefix_indexes' => true,
         ],
 
-        'mongodb' => [
-            'driver' => 'mongodb',
-            'dsn' => env('DB_DSN'),
-            'database' => env('DB_DATABASE', 'homestead'),
-        ],
-
-
-
     ],
 
     /*
